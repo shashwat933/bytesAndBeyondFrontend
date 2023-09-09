9c3b43c01a42cb8e5dd22ec57ff96a0dcf8ea08b
Shashwat Gupta <shashwatgupta21054@gmail.com>
2023-09-09 22:37:12 +0530

    Minor updates

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import './BlogCard.css'
 
 
 export default function BlogCard({ title, description, image, username, time, id, isUser }) {
@@ -13,12 +13,13 @@ export default function BlogCard({ title, description, image, username, time, id
 
     return (
         <>
-            <div className="mobile2:w-size2 w-size1 mb-10 rounded overflow-hidden shadow-lg">
-                <div className='m-4 flex justify-between'> <div><div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-mine2 rounded-full ">
-                    <span className="font-medium text-white ">{username[0]}</span>
+            <div className="mobile2:w-size2    w-size1 mb-10 rounded overflow-hidden shadow-lg">
+                <div className='m-4 flex justify-between'> <div>
+                    <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-mine2 rounded-full ">
+                    <span className="font-medium capitalize text-white ">{username[0]}</span>
 
                 </div>
-                    <span className="ml-4 text-lg font-medium text-mine2 ">{username}</span></div>
+                    <span className="ml-4 capitalize text-lg font-medium  text-mine2 ">{username}</span></div>
 
                     {isUser && <div className='flex  items-center justify-between	'>
 
@@ -28,8 +29,8 @@ export default function BlogCard({ title, description, image, username, time, id
                 </div>
                 <img className="h-sizeOfImage w-full" src={image} alt="Technical Blog" />
                 <div className="px-6 py-4 ">
-                    <div className="font-bold text-xl mb-2">{title}</div>
-                    <p className="text-gray-700 line-clamp-4 whitespace-pre text-base">
+                    <div className="font-bold uppercase tracking-wide font-mine text-2xl  mb-2">{title}</div>
+                    <p className="text-gray-700  text-md line-clamp-5 whitespace-pre-line  ">
                         {description}
                     </p>
                 </div>
